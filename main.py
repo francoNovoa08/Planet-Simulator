b20d09f38ba7645849885926ae9ecc7447f5e0f8
Franco Novoa <franconovoa3@gmail.com>
2025-06-07 19:36:57 -0500

    Initial commit and setup

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
