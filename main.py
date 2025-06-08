5d4a1ad30c9bc40d4c251b5a947dbeab5ee78f58
Franco Novoa <franconovoa3@gmail.com>
2025-06-07 20:07:54 -0500

    Add sun

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
+    sun = Sun(0, 0, 23, YELLOW, 1.98892 * 10 ** 30)
+
+    planets = []
+
     while run:
         clock.tick(60)
-        #WINDOW.fill(WHITE)
-        #pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        sun.draw(WINDOW, HEIGHT, WIDTH)
+        for planet in planets:
+            planet.draw(WINDOW, HEIGHT, WIDTH)
+        pygame.display.update()
 
     pygame.quit()
 
