d628d8c90f4920b2cdceeace195a3594d48f201e
Franco Novoa <franconovoa3@gmail.com>
2025-06-07 20:16:39 -0500

    Add Mars

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from Body import Planet, Sun
 
 pygame.init()
 
-WIDTH, HEIGHT = 650, 650
+WIDTH, HEIGHT = 800, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (80, 120, 255)
+RED = (255, 39, 50)
 
 
 def main():
@@ -21,7 +22,9 @@ def main():
 
     earth = Planet(-1 * Planet.AU, 0, 12, BLUE, 5.9742 * 10**24)
 
-    planets = [earth]
+    mars = Planet(-1.524 * Planet.AU, 0, 8, RED, 6.39 * 10**23)
+
+    planets = [earth, mars]
 
     while run:
         clock.tick(60)
