318b750578f61f0b86f355aa27126fa794d902fb
Franco Novoa <franconovoa3@gmail.com>
2025-06-07 19:53:28 -0500

    Set up Planet class

--- Planet.py
+++ Planet.py
@@ -1,5 +1,20 @@
-def main():
+class Planet:
+    AU = 149597870.7
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1 AU = 100 px
+    TIMESTEP = 3600 * 24 # 1 day
 
+    def __init__(self, x, y, radius, colour, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.colour = colour
+        self.mass = mass
 
-if __name__ == "__main__":
-    main()
+        self.orbit = []
+
+        self.sun = False
+        self.distance_from_sun = 0
+
+        self.v_x = 0
+        self.v_y = 0
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import Planet
 
 pygame.init()
 
