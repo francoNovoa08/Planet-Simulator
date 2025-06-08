c12e444428a78ef8e8703f4db56706008995dad5
Franco Novoa <franconovoa3@gmail.com>
2025-06-07 19:42:59 -0500

    Set up pygame and add Planet.py

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ import math
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 600, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
+
+WHITE = (255, 255, 255)
 
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
+        #WINDOW.fill(WHITE)
+        #pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
     pygame.quit()
+
+if __name__ == "__main__":
+    main()
